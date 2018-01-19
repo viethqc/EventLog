1123bd82f8f37fc35e21e8bc8e448295b640cf6e
VietHQc <hoangvietbk2.06@gmail.com>
2018-01-19 17:14:06 +0700

    read eventdata

--- Eventlog/Main.cs
+++ Eventlog/Main.cs
@@ -7,13 +7,15 @@ using System.Diagnostics;
 using Microsoft.Win32;
 using System.IO;
 using System.Diagnostics.Eventing.Reader;
+using System.Xml;
 
 namespace Eventlog
 {
     class Test
     {
         static void Main()
-        {
+        {  
+
             string logType = "Microsoft-Windows-Sysmon/Operational";
             string query = "*[EventData/*]";
             EventLogReader elReader;
@@ -32,12 +34,40 @@ namespace Eventlog
             }
 
             int count = 0;
+            XmlDocument doc = new XmlDocument();
+            XmlNodeList nodeList = null;
+            XmlNode nodeEventData = null;
+            XmlNode nodeData = null;
+            XmlNodeList nodeListData = null;
+            string sAttributeName = "";
+            string sInnerText = "";
+
+            string text = "";
             for (EventRecord eventInstance = elReader.ReadEvent(); eventInstance != null; eventInstance = elReader.ReadEvent())
             {
-                string kk = eventInstance.ToXml();
+                text = eventInstance.ToXml();
+                doc.LoadXml(text);
+                nodeList = doc.GetElementsByTagName("EventData");
+                if (nodeList.Count != 1)
+                {
+                    continue;
+                }
+                nodeEventData = nodeList.Item(0);
+                nodeListData = nodeEventData.ChildNodes;
+                for (int i = 0; i < nodeListData.Count; i++)
+                {
+                    nodeData = nodeListData.Item(i);
+                    sAttributeName = nodeData.Attributes["Name"].Value;
+                    sInnerText = nodeData.InnerText;
+                    int a = 9;
+                    a++;
+                }
+
                 string b = "d";
                 count++;
             }
+
+            return;
         }
     }
 }
